b68010e5c16a1e24e796d65f6b8f42b514661383
MukulVerma14 <mukul23102@iiitnr.edu.in>
2026-06-17 17:40:22 +0530

    Initial Commit

---
+++ src/main/java/com/example/ksep/KsepApplication.java
@@ -0,0 +1,13 @@
+package com.example.ksep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KsepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KsepApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ksep/KsepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ksep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KsepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
